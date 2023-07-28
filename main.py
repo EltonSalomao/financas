fecdaada1869836fc638006d9a6d76c6bb5da429
EltonSalomao <140858951+EltonSalomao@users.noreply.github.com>
2023-07-28 20:15:01 -0300

    Adicão da função de cadastrar cliente

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-db_connection = mysql.connector.connect(host='192.168.188.165', port='3306', user='admin', password='admin', database='financias') # Abrindo conexão com o banco de dados
-cursor = db_connection.cursor() # Cria uma variável cursor que fica com o retorno da execução da query
-nome = input("Digite seu nome: ") # Usuário digita o nome
-cursor.execute(f"INSERT INTO Cliente(nome,cpf,saldo) VALUES ('{nome}','45678912399',0)") # Deixamos a query que salva no banco de dados pronta pra ser chamada
-db_connection.commit() # Enviamos todas as querys prontas pra o banco
 
 def cadastrar_cliente():
     nome=input("Digite o seu nome: ")
     cpf=input("Digite o seu cpf: ")
-    valor=float(input("Digite o valor do seu produto: "))
+    db_connection = mysql.connector.connect(host='192.168.188.165', port='3306', user='admin', password='admin', database='financias') # Abrindo conexão com o banco de dados
+    cursor = db_connection.cursor()
+    cursor.execute(f"INSERT INTO Cliente(nome,cpf,saldo) VALUES ('{nome}',{cpf},0)") # Deixamos a query que salva no banco de dados pronta pra ser chamada
+    db_connection.commit() # Enviamos todas as querys prontas pra o banco
 
 
-print(cadastrar_cliente())
+cadastrar_cliente()
