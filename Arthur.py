ca60a04c75d16add787e9a4083a5b38375a43d47
well-programador7 <140861024+well-programador7@users.noreply.github.com>
2023-08-02 21:42:04 -0300

    arthur

---
+++ Arthur.py
@@ -0,0 +1,23 @@
+import mysql.connector
+def listar_cliente():
+    db_connection = mysql.connector.connect(host='192.168.188.165', port='3306', user='admin', password='admin', database='financias') # Abrindo conexão com o banco de dados
+    cursor = db_connection.cursor()
+    cursor.execute(f"SELECT * from Cliente") # Deixamos a query que salva no banco de dados pronta pra ser chamada
+    resultado=cursor.fetchall()
+    for cliente in resultado:
+        print(cliente)
+    db_connection.close()
+
+def deletar_cliente():
+    nome=input("Digite o seu nome: ")
+    cpf=input("Digite o seu cpf: ")
+    db_connection = mysql.connector.connect(host='192.168.188.165', port='3306', user='admin', password='admin', database='financias') # Abrindo conexão com o banco de dados
+    cursor = db_connection.cursor()
+    cursor.execute(f"INSERT INTO Cliente(nome,cpf,saldo) VALUES ('{nome}',{cpf},0)") # Deixamos a query que salva no banco de dados pronta pra ser chamada
+    while True:
+        listar_cliente()
+    
+
+    
+        db_connection.commit() # Enviamos todas as querys prontas pra o banco
+        db_connection.close()
