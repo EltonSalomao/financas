617881663f8073f3d418e56079ed9cef79f5ecad
EltonSalomao <140858951+EltonSalomao@users.noreply.github.com>
2023-08-03 20:29:15 -0300

    Funções deletar e listar categoria

--- main.py
+++ main.py
@@ -39,22 +39,38 @@ def listar_lançamento():
         print(lançamento)
     db_connection.close()
 
-def del_lançamento():
+def deletar_lançamento():
     listar_lançamento()
-    id = int(input("Digite o ID: "))
+    id = int(input("Digite o ID do lançamento a ser excluído: "))
     db_connection = mysql.connector.connect(host='192.168.188.165', port='3306', user='admin', password='admin', database='financias') # Abrindo conexão com o banco de dados
     cursor = db_connection.cursor()
     cursor.execute(f"DELETE from Lancamento WHERE id_lancamento={id}") # Deixamos a query que salva no banco de dados pronta pra ser chamada
-    resultado=cursor.fetchall()
-    for lançamento in resultado:
-        print(lançamento)
-        db_connection.close()
+    db_connection.commit() # Enviamos todas as querys prontas pra o banco
+    db_connection.close()
 
 def cadastrar_categoria():
     nome=input("Digite o nome da categoria: ")
     db_connection = mysql.connector.connect(host='192.168.188.165', port='3306', user='admin', password='admin', database='financias') # Abrindo conexão com o banco de dados
     cursor = db_connection.cursor()
     cursor.execute(f"INSERT INTO Categoria(nome) VALUES ('{nome}')") # Deixamos a query que salva no banco de dados pronta pra ser chamada
+    db_connection.commit() # Enviamos todas as querys prontas pra o banco
+    db_connection.close()
+
+def listar_categorias():
+    db_connection = mysql.connector.connect(host='192.168.188.165', port='3306', user='admin', password='admin', database='financias') # Abrindo conexão com o banco de dados
+    cursor = db_connection.cursor()
+    cursor.execute(f"SELECT * from Categoria") # Deixamos a query que salva no banco de dados pronta pra ser chamada
+    resultado=cursor.fetchall()
+    for categoria in resultado:
+        print(categoria)
+    db_connection.close()
+
+def deletar_categoria():
+    listar_categorias()
+    id_categoria=int(input("Digite o ID da categoria a ser deletada: "))
+    db_connection = mysql.connector.connect(host='192.168.188.165', port='3306', user='admin', password='admin', database='financias') # Abrindo conexão com o banco de dados
+    cursor = db_connection.cursor()
+    cursor.execute(f"DELETE from Categoria WHERE id_categoria={id_categoria}") # Deixamos a query que salva no banco de dados pronta pra ser chamada
     db_connection.commit() # Enviamos todas as querys prontas pra o banco
     db_connection.close()
 
@@ -66,23 +82,23 @@ def menu_geral():
     print("3 - Menu Categoria")
     try:
         op=input("Digite dentre as opções a seguir: ")
-        if op==1:
+        if op=="1":
             print("1 - Cadastrar Cliente")
             print("2 - Deletar Cliente")
             print("3 - Listar Cliente")
             opcli=input("Digite a opção que você deseja: ")
-            if opcli==1:
+            if opcli=="1":
                 cadastrar_cliente()
-            elif opcli==2:
+            elif opcli=="2":
                 deletar_cliente()
-            elif opcli==3:
+            elif opcli=="3":
                 listar_cliente()
 
-        elif op==2:
+        elif op=="2":
             print("1 - Cadastrar Lançamento")
             print("2 - Deletar Lançamento")
             print("3 - Listar Lançamentos")
-        elif op==3:
+        elif op=="3":
             print("1 - Cadastrar Categoria")
             print("2 - Deletar Categoria")
             print("3 - Listar Categorias")
@@ -93,7 +109,7 @@ def menu_geral():
     db_connection.close()
 
 
-deletar_cliente()
+deletar_categoria()
 
 # teste
 # teste 2
